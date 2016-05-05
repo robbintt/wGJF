45d1c55a9fe4a1ed8b44911101481a029d4fa7e2
Trent Robbins <robbintt@gmail.com>
2016-05-05 15:58:30 -0700

    Wrote a recursive method for searching and added some logging. This doesn't store the resulting data anywhere. it should probably all be stored in a sqlite table. This could lighten the load on subsequent searches where 'recent information' could be reused.

--- wikirobot.py
+++ wikirobot.py
@@ -1,30 +1,149 @@
 """ A little sample wiki robot for reading information
 
+Observation: Links in a wiki are one-way. It is not necessary to subtract source when
+                traversing links by depth.
+
+
+
+Naming Idea: "Depth Charge" - because it's cool. Also we are doing a depth based search.
 """
 import requests
 
+import logging
+
+
+LOG_FILENAME = "debug_log.txt"
+logging.basicConfig(filename=LOG_FILENAME, level=logging.DEBUG)
+
+DEPTH = 3
+TRAVERSAL_SPEED_S = 0.3
+
 TARGET_WIKI_URL = "https://en.wikipedia.org/w/api.php"
-
-endpoint = dict()
-
-# endpoint static setup
-endpoint['action'] = 'query'
-endpoint['format'] = 'json'
-endpoint['titles'] = 'Main Page'
-#endpoint['prop'] = 'revisions'
-endpoint['rvprop'] = 'content'
 
 headers = dict()
 headers['user-agent'] = 'wiki-shallow-relation-graph/0.0.1'
 
+# used to track unrelated links and reduce traversals. See recursive base case.
+EXHAUSTED_TITLE_DEPTHS = dict()
+
+
+def endpoint_initializer(title, pllimit='5000', prop='links', action='query', _format='json'):
+    """ Set up an endpoint with some defaults and a article title.
+    """
+
+    # this formatting is a little easier on the eyes for simple settings dicts.
+    endpoint = dict()
+    endpoint['action'] = action
+    endpoint['format'] = _format
+    endpoint['titles'] = title
+    endpoint['prop'] = prop
+    endpoint['pllimit'] = pllimit
+    #endpoint['prop'] = 'revisions'
+    #endpoint['rvprop'] = 'content'
+
+    return endpoint
+
+
+def get_exit_links(url, headers, endpoint):
+    """ Build the request, hit the URL specified in params, and 
+    """
+    r = requests.get(url, headers=headers, params=endpoint)
+
+    # munge exit links from the json response
+    if r.status_code == 200:
+        exit_links = list()
+        entry_info = r.json()
+        for k, v in entry_info['query']['pages'].iteritems():
+            if len(entry_info['query']['pages'].keys()) > 1:
+                logging.debug("Why was more than one page returned in this query?")
+            for link in v['links']:
+                if link['ns'] == 0:
+                    exit_links.append(link['title'])
+
+        import json
+        with open('python_page.json', 'w') as f:
+            json.dump(entry_info, f)
+
+    else: 
+        logging.debug("Request failed at: {}, error {}.".format(url, r.status_code))
+
+    if len(exit_links) > 5000:
+        logging.debug("More than 5000 titles at: {}, a longer handling script should be created.".format(url))
+
+    return exit_links
+
+
+def collect_routes(depth_counter, next_title, title_route=tuple()):
+    """ Recursively collect each route back to the source material.
+
+    This recursive function traverses deep links in wikipedia and
+        searches for links back to the source material.
+    """
+    title_route += (next_title,)
+
+    print("Traversing Route: {}".format(title_route))
+
+    import time; time.sleep(TRAVERSAL_SPEED_S)
+
+    if depth_counter > 0:
+        endpoint = endpoint_initializer(next_title)
+
+        exit_links = get_exit_links(TARGET_WIKI_URL, headers, endpoint)
+
+        # record routes that return to the source title.
+        if ROOT_TITLE in exit_links:
+            print(title_route, depth)
+            logging.debug("Return route found at depth {}: {}".format(depth, title_route))
+
+        for title in exit_links:
+            # keep digging unless we hit the source title.
+
+            # depth will never be < 1 here.
+            # the depth_counter check skips queries if depth_counter or shallower was already traversed according to EXHAUSTED_TITLE_DEPTHS.
+            if depth_counter > EXHAUSTED_TITLE_DEPTHS.get(title, -1) and title != title_route[0]:
+                collect_routes((depth_counter-1), title, title_route)
+
+
+            """
+            One way we can prevent repeat traversals is to add to the global exhaustion list only from the nearest nodes to the source node.
+
+            We could actually do an exhaustion dictionary. If an item is exhausted we record the depth it is exhausted at.
+            If it is tested at a higher depth we then refer to the dictionary and discover we should test it some more.
+            """
+            # add higher depth counters when traversing back up the recursion chain
+            if next_title in EXHAUSTED_TITLE_DEPTHS:
+                if EXHAUSTED_TITLE_DEPTHS[next_title] < depth_counter:
+                    EXHAUSTED_TITLE_DEPTHS[next_title] = depth_counter
+            else:
+                EXHAUSTED_TITLE_DEPTHS[next_title] = depth_counter
+
+    else:
+        # base case
+        # depth counter is zero, title may not yet be in EXHAUSTED_TITLE_DEPTHS
+        if next_title in EXHAUSTED_TITLE_DEPTHS:
+            if EXHAUSTED_TITLE_DEPTHS[next_title] < depth_counter:
+                EXHAUSTED_TITLE_DEPTHS[next_title] = depth_counter
+        else:
+            EXHAUSTED_TITLE_DEPTHS[next_title] = depth_counter
+
+    return
+
 
 if __name__ == "__main__":
+    """
 
-    r = requests.get(TARGET_WIKI_URL, headers=headers, params=endpoint)
+    What kind of result do we want?
 
-    if r.status_code == "200":
+    A list of tuples with the title and the depth would be cool.
+    However, we are supposed to return the whole route.
+    So it would be more like a list of titles, starting with the source
+    and ending with the title that references the source.
 
-        entry_info = r.json()
+    This is a good place for recursion.
+    Spawn a new controller for each item in the exit_links list.
 
-        print(entry_info)
+    """
+    ROOT_TITLE = "Python_(programming_language)"
+
+    collect_routes(DEPTH, ROOT_TITLE)
 
