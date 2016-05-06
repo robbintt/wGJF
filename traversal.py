29fd19b831a279a2af8546b1f5c68cdb8d41aafd
Trent Robbins <robbintt@gmail.com>
2016-05-06 01:38:26 -0700

    Fixed up comments and spacing.

--- traversal.py
+++ traversal.py
@@ -1,12 +1,4 @@
 """ A depth traversal/search of internal wiki links from a beginning article.
-
-Observation: Links in a wiki are one-way. It is not necessary to subtract 
-                source when traversing links by depth.
-
-
-
-Naming Idea: "Depth Charge" - because it's cool. Also we are doing a depth 
-                based search.
 """
 import requests
 import logging
@@ -19,17 +11,29 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+
+LOG_FILENAME = "debug.log"
+logging.basicConfig(filename=LOG_FILENAME, level=logging.DEBUG)
+
+DEPTH = 2
+TRAVERSAL_SPEED_S = 0.3
+
+TARGET_WIKI_URL = "https://en.wikipedia.org/w/api.php"
+
+# easy to read header config section
+headers = dict()
+headers['user-agent'] = 'wGJF/0.0.1'
+
+
 Base = declarative_base()
 
 class Links(Base):
     """ 
-    Timestamps are for internal use. Use unix epoch time.
+    Timestamps are for cache management. Use unix epoch time.
 
-    Wikipedia Title Limit:
-    https://en.wikipedia.org/wiki/Wikipedia:Naming_conventions_(technical_restrictions)#Title_length
+    Wikipedia Title Limit is 256 characters: https://en.wikipedia.org/wiki/Wikipedia:Naming_conventions_(technical_restrictions)#Title_length
 
     256*5000=1,280,000
-
     """
     __tablename__ = 'links'
 
@@ -51,22 +55,6 @@ Session.configure(bind=engine)
 session = Session()
 
 
-LOG_FILENAME = "debug.log"
-logging.basicConfig(filename=LOG_FILENAME, level=logging.DEBUG)
-
-DEPTH = 2
-TRAVERSAL_SPEED_S = 0.3
-
-TARGET_WIKI_URL = "https://en.wikipedia.org/w/api.php"
-
-# easy to read header config section
-headers = dict()
-headers['user-agent'] = 'wGJF/0.0.1'
-
-# used to track unrelated links and reduce traversals. See recursive base case.
-EXHAUSTED_TITLE_DEPTHS = dict()
-
-
 def endpoint_initializer(title, pllimit='5000', prop='links', action='query', _format='json'):
     """ Set up an endpoint with some defaults and an article title.
     """
@@ -78,8 +66,6 @@ def endpoint_initializer(title, pllimit='5000', prop='links', action='query', _f
     endpoint['titles'] = title
     endpoint['prop'] = prop
     endpoint['pllimit'] = pllimit
-    #endpoint['prop'] = 'revisions'
-    #endpoint['rvprop'] = 'content'
 
     return endpoint
 
@@ -101,7 +87,6 @@ def get_exit_links(url, headers, endpoint):
                 for link in v['links']:
                     if link['ns'] == 0:
                         exit_links.append(link['title'])
-
     else: 
         logging.debug("Request failed at: {}, error {}.".format(url, r.status_code))
 
@@ -112,18 +97,14 @@ def get_exit_links(url, headers, endpoint):
 
 
 def collect_routes(depth_counter, next_title, title_route=tuple()):
-    """ Recursively collect each route back to the source material.
-
-    This recursive function traverses deep links in wikipedia and
-        searches for links back to the source material.
+    """ Recursively collect links a certain depth from next_title
     """
     title_route += (next_title,)
 
-
     instance = session.query(Links).filter(Links.page == next_title).first()
+
     if instance:
         exit_links = json.loads(instance.links)
-        print(exit_links)
 
         """
         # code to update cache if triggered, not currently used
@@ -136,7 +117,6 @@ def collect_routes(depth_counter, next_title, title_route=tuple()):
             session.rollback()
         """
     else:
-
         print("Traversing Route: {}".format(title_route))
         time.sleep(TRAVERSAL_SPEED_S)
 
@@ -155,62 +135,34 @@ def collect_routes(depth_counter, next_title, title_route=tuple()):
         except:
             session.rollback()
 
-    # record routes that return to the source title.
+    # SIMPLE RESULTS: record routes that return to the source title.
     if TARGET_TITLE in exit_links:
-        print(depth_counter, title_route)
         logging.debug("Return route found at depth {}: {}".format(depth_counter, title_route))
 
     if depth_counter > 0:
-
         for title in exit_links:
             # keep digging unless we hit the source title.
-
-            # depth will never be < 1 here.
-            # the depth_counter check skips queries if depth_counter or 
-            # shallower was already traversed according to EXHAUSTED_TITLE_DEPTHS.
-            if depth_counter > EXHAUSTED_TITLE_DEPTHS.get(title, -1) and title != title_route[0]:
+            if title != title_route[0]:
                 collect_routes((depth_counter-1), title, title_route)
-
     else:
         # base case
         pass
-
-    """
-    One way we can prevent repeat traversals is to add to the global 
-    exhaustion list only from the nearest nodes to the source node.
-
-    We could actually do an exhaustion dictionary. If an item is exhausted we 
-    record the depth it is exhausted at.
-
-    If it is tested at a higher depth we then refer to the dictionary and 
-    discover we should test it some more.
-    """
-    # add depth counters when traversing back up the recursion chain
-    if next_title in EXHAUSTED_TITLE_DEPTHS:
-        if EXHAUSTED_TITLE_DEPTHS[next_title] < depth_counter:
-            EXHAUSTED_TITLE_DEPTHS[next_title] = depth_counter
-    else:
-        EXHAUSTED_TITLE_DEPTHS[next_title] = depth_counter
 
     return
 
 
 if __name__ == "__main__":
     """
-    What kind of result do we want?
+    The most powerful tool is currently results cacheing.
 
-    A list of tuples with the title and the depth would be cool.
-    However, we are supposed to return the whole route.
-    So it would be more like a list of titles, starting with the source
-    and ending with the title that references the source.
+    The results we get inside the recursive function include any article
+    that links back to the TARGET_TITLE and its depth from the ROOT_TITLE.
 
-    This is a good place for recursion.
-    Spawn a new controller for each item in the exit_links list.
-
+    This could be annotated somewhere, but this general tool is much
+    more powerful than this special use.
     """
     # use care, underscores in the url need to be spaces in the title for the comparison to work.
     ROOT_TITLE = "Python (programming language)"
     TARGET_TITLE = ROOT_TITLE
 
     collect_routes(DEPTH, ROOT_TITLE)
-
